49e68c427097a3c0b888b5742da1336f6d2afddd
cemtastan <cemtastan1234@gmail.com>
2020-03-02 19:23:07 +0200

    turkish practice

---
+++ Ders1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	cout << "bu dunyayi daha iyi bir yer haline getirecegim ulan !!" << endl;
+	cout << "bu dunyayi daha iyi bir yer haline getirecegim ulan !!";
+
+	return 0; 
+}
